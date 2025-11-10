9f840e3d383376ed15fba40da2d77492da9fb044
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-11-10 15:11:36 -0600

    Update UserService.java

--- ServiceLayer&DependencyInjection/UserService.java
+++ ServiceLayer&DependencyInjection/UserService.java
@@ -1,1 +1,11 @@
+package com.example.hellospring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String createGreeting(String name) {
+       return "Hello " + name +" from Service";
+    }
+}
 
