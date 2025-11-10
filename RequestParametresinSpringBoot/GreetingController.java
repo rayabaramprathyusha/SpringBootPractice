75ccbad61ea5992cb4aba865fab1aa3fb05d50b3
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-11-10 15:17:53 -0600

    Update GreetingController.java

--- RequestParametresinSpringBoot/GreetingController.java
+++ RequestParametresinSpringBoot/GreetingController.java
@@ -1,1 +1,24 @@
+package com.example.hellospring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController{
+
+    //using path variable
+    @GetMapping("/greet/{name}")
+    public String greetUserPath(@PathVariable String name){
+        return "Hello "+name +" Via Pathvariable";
+    }
+
+    //using request parameter
+    @GetMapping("/greet")
+    public String greetUserParam(@RequestParam String name){
+        return "Hello "+name +" Via Param";
+    }
+
+}
 
