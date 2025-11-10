4e7d98e619624881a5c8ff7d86759d64cefcd543
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-11-10 15:10:23 -0600

    Update EmployeesController.java

--- HandlingList&DynamicData/EmployeesController.java
+++ HandlingList&DynamicData/EmployeesController.java
@@ -1,1 +1,19 @@
 
+package com.example.hellospring;
+import java.util.*;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeesController {
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees(){
+        return List.of(
+            new Employee("Vinny",87000),
+            new Employee("Jinny",25000),
+            new Employee("Jonny",9000));
+    }
+}
