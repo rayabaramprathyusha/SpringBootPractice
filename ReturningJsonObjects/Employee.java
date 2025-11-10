9bfb77c74de0bf5bd99a8e6762aac357ebbed553
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-11-10 15:13:08 -0600

    Update Employee.java

--- ReturningJsonObjects/Employee.java
+++ ReturningJsonObjects/Employee.java
@@ -1,1 +1,16 @@
+package com.example.hellospring;
+
+public class Employee {
+
+    public String name;
+    public double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {return name;}
+    public double getSalary() {return salary;}
+}
 
